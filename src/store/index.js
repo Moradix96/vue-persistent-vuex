edccf279624ea2075547cd9341964568acbde9bc
Sina <Moradix96@gmail.com>
2022-02-17 10:10:02 +0330

    Auto store vuex state in localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,26 @@ const store = new Vuex.Store({
         count: 1
     },
     mutations: {
+        initialiseStore(state) {
+            // Check if the ID exists
+            if(localStorage.getItem('store')) {
+                // Replace the state object with the stored item
+                this.replaceState(
+                    Object.assign(state, JSON.parse(localStorage.getItem('store')))
+                );
+            }
+        },
         increase: (state) => {
             state.count++;
         }
     },
     getters: {}
+});
+
+// Subscribe to store updates
+store.subscribe((mutation, state) => {
+    // Store the state object as a JSON string
+    localStorage.setItem('store', JSON.stringify(state));
 });
 
 export default store
